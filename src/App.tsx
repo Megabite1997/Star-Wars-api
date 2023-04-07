02cd7ac5279c2b3b6eb16e7dee71101499650c42
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-07 18:33:34 +0700

    🎨 Handle fetch error

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ interface moviesType {
 function App() {
   const [movies, setMovies] = useState<moviesType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
   async function fetchMoviesHandler(): Promise<void> {
     // fetch("https://swapi.dev/api/films/", {
@@ -51,17 +52,48 @@ function App() {
     //   });
     setIsLoading(true);
 
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData: movieDataType) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-      };
-    });
-    setMovies(transformedMovies);
+    try {
+      // Fetch API doesn't treat error status codes, it will not throw a technical error.
+      const response = await fetch("https://swapi.dev/api/film/");
+
+      // Generate and throwour own error.
+      if (!response.ok) {
+        throw new Error(`Something went wrong ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData: movieDataType) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+        setError(error.message);
+      } else {
+        console.log("Unexpected error", error);
+      }
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>Found no movies</p>;
+
+  if (movies.length > 0) {
+    content = <MovieList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -74,11 +106,7 @@ function App() {
         </div>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MovieList movies={movies} />}
-        {!isLoading && movies.length === 0 && (
-          <p className="no-movies">There are no movies</p>
-        )}
-        {isLoading && <p className="loading">Loading...</p>}
+        <div className="content">{content}</div>
       </section>
     </div>
   );
